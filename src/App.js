9e442174ae0b5a4fa1092f55307beea2da0c1e21
Michael Ugochukwu <mark.omulu@gmail.com>
2022-08-11 19:06:33 +0100

    Implement saving messages in local storage

--- src/App.js
+++ src/App.js
@@ -4,21 +4,57 @@ import { useEffect, useState } from "react";
 export default function App() {
   const [name, setname] = useState();
   const [message, setMessage] = useState("");
+  const [chatPage, setChatPage] = useState();
 
-    const handleName = (e) => {
+  const handleClick = () => {
+    setChatPage(true);
+  };
+  
+  const handleName = (e) => {
     setname(e.target.value);
+  };
+    useEffect(() => {
+    if (!JSON.parse(window.localStorage.getItem("chat"))) {
+      window.localStorage.setItem("chat", JSON.stringify([]));
+    }
+  	});
+  
+  const handleSend = () => {
+    const prev = JSON.parse(window.localStorage.getItem("chat"));
+    const newState = [
+      ...prev,
+      {
+        name,
+        message
+      }
+    ];
+    window.localStorage.setItem("chat", JSON.stringify(newState));
   };
   return (
     <div className="App">
-        <div>
+      {chatPage ? (
+        <div className="wrapper">
+          <div className="chat">
+            <input
+              value={message}
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+            />
+            <button onClick={handleSend}>Send Message </button>
+          </div>
+        </div>
+      ) : (
+        <div className="wrapper">
           <div>
-            <label>Name</label>
+            <label>Name: </label>
             <input onChange={handleName} />
-            <div>
-              <button>Sign In </button>
+            <div className="signIn">
+              <button onClick={handleClick}>Sign In </button>
             </div>
           </div>
         </div>
+      )}
     </div>
   );
 }
